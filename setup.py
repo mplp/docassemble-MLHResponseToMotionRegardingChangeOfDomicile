4f63391ad17664ab4c1a355b68e8f7902b518e7b
Ramiro Lorca <110207826+RamiLorca@users.noreply.github.com>
2024-06-21 12:20:50 -0400

    Integrated remaining intro/outro changes from Emily's PR

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.MLHResponseToMotionRegardingChangeOfDomicile',
       version='0.0.1',
       description=('A Response to a Motion Regarding Change of Domicile for use in Michigan Family Courts'),
-      long_description='# docassemble.MLHResponseToMotionRegardingChangeOfDomicile\n\nA response to a motion regarding change of domicile in Michigan.\n\n## Author\n\nPratibha Bharti\n\n',
+      long_description='# docassemble.MLHResponseToMotionRegardingChangeOfDomicile\r\n\r\nA response to a motion regarding change of domicile in Michigan\r\n\r\n## Author\r\n\r\n- Pratibha Bharti\r\n',
       long_description_content_type='text/markdown',
       author='Pratibha',
       author_email='pratibha.mobilefirst@gmail.com',
